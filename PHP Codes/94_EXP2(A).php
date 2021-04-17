98ceda1720ead0c63b8a10de39e9d571845be544
SahilMe02 <64036377+SahilMe02@users.noreply.github.com>
2021-04-17 21:38:44 +0530

    Add files via upload

---
+++ PHP Codes/94_EXP2(A).php
@@ -0,0 +1,12 @@
+<?php
+echo"<br><center><h1> Exp2(a)(94) </h1></center>";
+$n = 6;
+if($n % 2 ==0)
+{
+    echo"the number is even.";
+}
+else
+{
+    echo "the number is odd.";
+}
+?>
---
+++ PHP Codes/94_EXP2(B).php
@@ -0,0 +1,17 @@
+<?php
+echo "<br><center><h1> Exp2(B)(94) </h1></center>";
+$n= -5;
+if($n>0)
+{
+    echo "the number $n is positive.";
+}
+elseif ($n<0)
+{
+    echo "the number $n is negative";
+
+}
+else
+{
+    echo "the number is 0";
+}
+?>
